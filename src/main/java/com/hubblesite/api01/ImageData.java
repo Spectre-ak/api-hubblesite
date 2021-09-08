8d85ade366e18650feef1932c5ad0f5a11b105dd
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-09-08 21:04:31 +0530

    Update ImageData.java

--- src/main/java/com/hubblesite/api01/ImageData.java
+++ src/main/java/com/hubblesite/api01/ImageData.java
@@ -3,12 +3,16 @@ package com.hubblesite.api01;
 import java.util.Scanner;
 
 import org.json.JSONArray;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageData {
 
-	
+	@GetMapping("/")
+	public Object index(){
+		return "/";
+	}
 
 }
