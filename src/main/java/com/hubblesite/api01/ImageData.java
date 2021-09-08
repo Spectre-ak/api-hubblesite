bedb1175c9aa3a3dee21ede1c7ed890f0bcccc96
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-09-08 20:52:34 +0530

    removed static files

--- src/main/java/com/hubblesite/api01/ImageData.java
+++ src/main/java/com/hubblesite/api01/ImageData.java
@@ -8,20 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageData {
+
 	
-	private JSONArray jsonArray;
-	public ImageData() {
-		Scanner scanner=new Scanner(this.getClass().getClassLoader().getResourceAsStream("imgArray.json"));
-		String jsonAsString="";
-		while(scanner.hasNextLine()) {
-			jsonAsString+=scanner.nextLine();
-		}
-		this.jsonArray=new JSONArray(jsonAsString);
-	}
-	
-	@GetMapping("/img/all")
-	public Object AllImages() {
-		return jsonArray.toString(3);
-	}
 
 }
--- src/main/java/com/hubblesite/api01/VidData.java
+++ src/main/java/com/hubblesite/api01/VidData.java
@@ -7,20 +7,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class VidData {
 	
-	private JSONArray jsonArray;
 	
-	public VidData() {
-		// TODO Auto-generated constructor stub
-		Scanner scanner=new Scanner(this.getClass().getClassLoader().getResourceAsStream("vidArray.json"));
-		String jsonAsString="";
-		while(scanner.hasNextLine()) {
-			jsonAsString+=scanner.nextLine();
-		}
-		this.jsonArray=new JSONArray(jsonAsString);
-	}
-	
-	@GetMapping("/vid/all")
-	public Object getAllVids() {
-		return this.jsonArray.toString(3);
-	}
 }
