fed8ffeeb848aa5a5add143f23ffbc031975715e
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-09-21 17:54:49 +0530

    Update ImageData.java

--- src/main/java/com/hubblesite/api01/ImageData.java
+++ src/main/java/com/hubblesite/api01/ImageData.java
@@ -1,14 +1,22 @@
 package com.hubblesite.api01;
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class ImageData {
 
 	private DBUtil dbUtil;
@@ -17,9 +25,16 @@ public class ImageData {
 		this.dbUtil=new DBUtil();
 		this.resc=this.dbUtil.getImages();
 	}
-	@GetMapping("/all/images")
-	public String index(){
-		return this.resc.toString(3);
+	
+	@RequestMapping(value = "/all/images", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ArrayList<String> index(){
+		ArrayList<String> arrayList=new ArrayList<>();
+		for(Object jsonObject:this.resc) {
+			arrayList.add(jsonObject.toString());
+			break;
+		}
+		
+		return arrayList;
 	}
 
 }
