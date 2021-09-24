6f831db346cc02a9bb41365ce46be390262cdb26
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-09-24 20:00:31 +0530

    Update ImageData.java

--- src/main/java/com/hubblesite/api01/ImageData.java
+++ src/main/java/com/hubblesite/api01/ImageData.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +29,19 @@ public class ImageData {
 	@RequestMapping(value = "/all/images", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String index(){
 		return this.resc.toString();
+	}
+	
+	@RequestMapping(value = "/images/tags/{tags}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String getImagesBasedOnTags(@PathVariable String tags) {
+		System.out.println(tags);
+		
+		for(Object obj:this.resc) {
+			obj=(JSONObject) obj;
+			System.out.println(obj);
+			break;
+		}
+		
+		return null;
 	}
 
 }
